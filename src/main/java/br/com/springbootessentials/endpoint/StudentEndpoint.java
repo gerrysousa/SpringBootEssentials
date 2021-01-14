5e2eadb5dfb7a308b5f095c0e2c8da8e469dcc1b
Gerry Sousa <gerry@ciandt.com>
2021-01-14 07:54:18 -0300

    Spring Boot Essentials 02 - @Component, @Autowired e @SpringBootApplication

--- src/main/java/br/com/springbootessentials/endpoint/StudentEndpoint.java
+++ src/main/java/br/com/springbootessentials/endpoint/StudentEndpoint.java
@@ -3,7 +3,10 @@ package br.com.springbootessentials.endpoint;
 import static java.util.Arrays.asList;
 
 import br.com.springbootessentials.model.Student;
+import br.com.springbootessentials.util.DateUtil;
+import java.time.LocalDateTime;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("student")
 public class StudentEndpoint {
+  @Autowired
+  private DateUtil dateUtil;
+
   @RequestMapping(method = RequestMethod.GET, path="/")
   public List<Student> listAll(){
+    System.out.println(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
     return asList(new Student("Zé"), new Student("Tião"));
   }
 }
