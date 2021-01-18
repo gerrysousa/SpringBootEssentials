f904d39282a9676a2d77ff5768e0cedd6841c46d
Gerry Sousa <gerry@ciandt.com>
2021-01-18 06:14:43 -0300

    Spring Boot Essentials 05 - Padrões REST e POST pt 01

--- src/main/java/br/com/springbootessentials/endpoint/StudentEndpoint.java
+++ src/main/java/br/com/springbootessentials/endpoint/StudentEndpoint.java
@@ -2,24 +2,45 @@ package br.com.springbootessentials.endpoint;
 
 import static java.util.Arrays.asList;
 
+import br.com.springbootessentials.error.CustomErrorType;
 import br.com.springbootessentials.model.Student;
 import br.com.springbootessentials.util.DateUtil;
 import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("student")
+@RequestMapping("students")
 public class StudentEndpoint {
-  @Autowired
+
   private DateUtil dateUtil;
 
-  @RequestMapping(method = RequestMethod.GET, path="/")
-  public List<Student> listAll(){
-    System.out.println(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-    return asList(new Student("Zé"), new Student("Tião"));
+  @Autowired
+  public StudentEndpoint(DateUtil dateUtil) {
+    this.dateUtil = dateUtil;
+  }
+
+  @RequestMapping(method = RequestMethod.GET, path = "/") // quando nao é passado o path="/"
+  public ResponseEntity<?> listAll() {
+    // System.out.println(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
+    return new ResponseEntity<>(Student.studentList, HttpStatus.OK);
+  }
+
+  @RequestMapping(method = RequestMethod.GET, path = "/{id}") // quando nao é passado o path="/"
+  public ResponseEntity<?> getStudentById(@PathVariable("id") int id) {
+    Student student = new Student();
+    student.setId(id);
+    int index = Student.studentList.indexOf(student);
+
+    if (index == -1) {
+      return new ResponseEntity<>(new CustomErrorType("Student not found"), HttpStatus.NOT_FOUND);
+    }
+    return new ResponseEntity<>(Student.studentList.get(index), HttpStatus.OK);
   }
 }
--- src/main/java/br/com/springbootessentials/model/Student.java
+++ src/main/java/br/com/springbootessentials/model/Student.java
@@ -1,13 +1,59 @@
 package br.com.springbootessentials.model;
 
+import static java.util.Arrays.asList;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class Student {
   private  String name;
+  private int id;
+  public static List<Student> studentList;
+
+  static {
+    studentRepository();
+  }
+
+  public Student(int id, String name) {
+    this(name);
+    this.id = id;
+  }
 
   public Student(String name) {
     this.name = name;
   }
 
   public Student() {
+  }
+
+  public static void studentRepository() {
+    studentList = new ArrayList<>(asList(new Student(1,"Zé"), new Student(2,"Tião")));
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Student student = (Student) o;
+    return id == student.id;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id);
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
   }
 
   public String getName() {
