96f708eba68129a0bae7be551b2c9da7d62e32f8
Gerry Sousa <gerry@ciandt.com>
2021-06-14 10:03:47 -0300

    Spring Boot Essentials 21 - Cliente Spring com RestTemplate pt 01 GET

---
+++ src/main/java/br/com/springbootessentials/javaclient/JavaSpringClientTest.java
@@ -0,0 +1,26 @@
+package br.com.springbootessentials.javaclient;
+
+import br.com.springbootessentials.model.Student;
+import java.util.Arrays;
+import java.util.List;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+public class JavaSpringClientTest {
+
+  public static void main(String[] args) {
+    RestTemplate restTemplate = new RestTemplateBuilder().rootUri("http://localhost:8080/v1/protected/students").basicAuthentication("admin", "123456").build();
+    Student student = restTemplate.getForObject("/{id}", Student.class,1);
+    System.out.println(student);
+    ResponseEntity<Student> forEntity = restTemplate.getForEntity("/{id}", Student.class,1);
+    System.out.println(forEntity.getBody());
+    Student [] students = restTemplate.getForObject("/", Student[].class);
+    System.out.println(Arrays.toString(students));
+    ResponseEntity<List<Student>> exchange = restTemplate.exchange("/", HttpMethod.GET, null, new ParameterizedTypeReference<List<Student>>() {
+    });
+    System.out.println(exchange.getBody());
+  }
+}
